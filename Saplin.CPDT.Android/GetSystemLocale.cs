b31110162883070d2a023c19db200e8ead12b1cf
Maxim Saplin <smaxmail@gmail.com>
2018-11-23 11:08:15 +0300

    Missing Android dependency for sys locale (forgot to commit)

---
+++ Saplin.CPDT.Android/GetSystemLocale.cs
@@ -0,0 +1,21 @@
+﻿using Saplin.CPDT.UICore;
+using Saplin.CPDT.UICore.Localization;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Saplin.CPDT.Droid.GetSystemLocale))]
+namespace Saplin.CPDT.Droid
+{
+    public class GetSystemLocale : IGetSystemLocale
+    {
+
+        public string GetLocale()
+        {
+            var l = Java.Util.Locale.Default.ToString();
+
+            if (l.StartsWith("ru")) return Locales.ru;
+            else if (l.StartsWith("fr")) return Locales.fr;
+
+            return Locales.en;
+        }
+    }
+}
