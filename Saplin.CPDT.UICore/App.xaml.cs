c3733b12eb85bc21ce720f0c44c0c84c0cd028b8
smax <smaxmail@gmail.com>
2019-06-01 02:25:43 +0300

    Async App Init

--- Saplin.CPDT.UICore/App.xaml.cs
+++ Saplin.CPDT.UICore/App.xaml.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
 using Saplin.CPDT.UICore.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -7,12 +10,27 @@ namespace Saplin.CPDT.UICore
 {
 	public partial class App : Application
 	{
-		public App ()
-		{
-			InitializeComponent();
+        Page page = null;
+        private Stopwatch sw;
+        private StringBuilder sb;
+        private Task task;
 
-			
-		}
+        public App ()
+		{
+            sw = new Stopwatch();
+            sb = new StringBuilder();
+
+            sw.Start();
+
+            task = Task.Run(() =>
+            {
+                InitializeComponent();
+                sb.AppendLine("InitializeComponent " + sw.ElapsedMilliseconds);
+                page = new MainPage(); sb.AppendLine("new MainPage() " + sw.ElapsedMilliseconds);
+            });
+
+            sb.AppendLine("Task.Run " + sw.ElapsedMilliseconds);
+        }
 
         public bool WhiteTheme
         {
@@ -24,7 +42,12 @@ namespace Saplin.CPDT.UICore
 
 		protected override void OnStart ()
 		{
-            MainPage = new MainPage();
+            task.Wait();
+            sb.AppendLine("Task.Wait " + sw.ElapsedMilliseconds);
+
+            MainPage = page;
+            sb.AppendLine("MainPage = page " + sw.ElapsedMilliseconds);
+            System.Diagnostics.Debug.WriteLine(sb);
         }
 
 		protected override void OnSleep ()
