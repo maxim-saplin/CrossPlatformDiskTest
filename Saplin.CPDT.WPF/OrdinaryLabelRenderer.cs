ec21242d0737788ded366d519fca2e0aed4b521b
smax <smaxmail@gmail.com>
2020-04-03 20:10:11 +0300

    2.2.3 WPF fixes

--- Saplin.CPDT.WPF/OrdinaryLabelRenderer.cs
+++ Saplin.CPDT.WPF/OrdinaryLabelRenderer.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Windows.Media;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.WPF;
+﻿//using System;
+//using System.Windows.Media;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.WPF;
 
-[assembly: ExportRenderer(typeof(Xamarin.Forms.Label), typeof(Saplin.CPDT.WPF.OrdinaryLabelRenderer))]
+////[assembly: ExportRenderer(typeof(Xamarin.Forms.Label), typeof(Saplin.CPDT.WPF.OrdinaryLabelRenderer))]
 
-namespace Saplin.CPDT.WPF
-{
-    public class OrdinaryLabelRenderer : LabelRenderer
-    {
-        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
-        {
-            base.OnElementChanged(e);
-            var label = (e.NewElement as Label);
+//namespace Saplin.CPDT.WPF
+//{
+//    public class OrdinaryLabelRenderer : LabelRenderer
+//    {
+//        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+//        {
+//            base.OnElementChanged(e);
+//            var label = (e.NewElement as Label);
 
-            // TODO - check the bug with custom fonts in Spans fixed and remove
-            if ((Control != null))
-            {
-                if (label.FormattedText != null && label.FormattedText.Spans != null && label.FormattedText.Spans.Count > 0 && Control.Inlines != null && Control.Inlines.Count == label.FormattedText.Spans.Count)
-                {
-                    var i = 0;
-                    foreach (var inl in Control.Inlines)
-                    {
-                        if (!string.IsNullOrEmpty(label.FormattedText.Spans[i].FontFamily))
-                        {
-                            inl.FontFamily = new FontFamily(new Uri("pack://application:,,,/"), label.FormattedText.Spans[i].FontFamily);
-                        }
+//            // TODO - check the bug with custom fonts in Spans fixed and remove
+//            if ((Control != null))
+//            {
+//                if (label.FormattedText != null && label.FormattedText.Spans != null && label.FormattedText.Spans.Count > 0 && Control.Inlines != null && Control.Inlines.Count == label.FormattedText.Spans.Count)
+//                {
+//                    var i = 0;
+//                    foreach (var inl in Control.Inlines)
+//                    {
+//                        if (!string.IsNullOrEmpty(label.FormattedText.Spans[i].FontFamily))
+//                        {
+//                            inl.FontFamily = new FontFamily(new Uri("pack://application:,,,/"), label.FormattedText.Spans[i].FontFamily);
+//                        }
 
-                        i++;
-                    }
-                }
-            }
-        }
-    }
-}
+//                        i++;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ Saplin.CPDT.WPF/TextBlockExtensions.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using Xamarin.Forms.Internals;
+using NativeSize = System.Windows.Size;
+using Label = Xamarin.Forms.Label;
+using Xamarin.Forms;
+using System.Windows;
+using System.Windows.Media;
+using System;
+
+namespace Sapin.CPDT.WPF
+{
+	internal static class TextBlockExtensions
+	{
+
+		public static double FindDefaultLineHeight(this TextBlock control, Inline inline)
+		{
+			control.Inlines.Add(inline);
+
+			control.Measure(new NativeSize(double.PositiveInfinity, double.PositiveInfinity));
+
+			var height = control.DesiredSize.Height;
+
+			control.Inlines.Remove(inline);
+			control = null;
+
+			return height;
+		}
+
+		public static void RecalculateSpanPositions(this TextBlock control, Label element, IList<double> inlineHeights)
+		{
+			if (element?.FormattedText?.Spans == null
+				|| element.FormattedText.Spans.Count == 0)
+				return;
+
+			var labelWidth = control.ActualWidth;
+
+			if (labelWidth <= 0 || control.Height <= 0)
+				return;
+
+			for (int i = 0; i < element.FormattedText.Spans.Count; i++)
+			{
+				var span = element.FormattedText.Spans[i];
+
+				var inline = control.Inlines.ElementAt(i);
+				var rect = inline.ContentStart.GetCharacterRect(LogicalDirection.Forward);
+				var endRect = inline.ContentEnd.GetCharacterRect(LogicalDirection.Forward);
+
+				var positions = new List<Rectangle>();
+
+
+				var defaultLineHeight = inlineHeights[i];
+
+				if (defaultLineHeight == 0) defaultLineHeight = 19.5;
+
+				var yaxis = rect.Top;
+				var lineHeights = new List<double>();
+				while (yaxis < endRect.Bottom)
+				{
+					double lineHeight;
+					if (yaxis == rect.Top) // First Line
+					{
+						lineHeight = rect.Bottom - rect.Top;
+					}
+					else if (yaxis != endRect.Top) // Middle Line(s)
+					{
+						lineHeight = defaultLineHeight;
+					}
+					else // Bottom Line
+					{
+						lineHeight = endRect.Bottom - endRect.Top;
+					}
+					lineHeights.Add(lineHeight);
+					yaxis += lineHeight;
+				}
+
+				((ISpatialElement)span).Region = Region.FromLines(lineHeights.ToArray(), labelWidth, rect.X, endRect.X + endRect.Width, rect.Top).Inflate(10);
+
+			}
+		}
+	}
+
+
+	internal static class AlignmentExtensions
+	{
+		internal static System.Windows.TextAlignment ToNativeTextAlignment(this Xamarin.Forms.TextAlignment alignment)
+		{
+			switch (alignment)
+			{
+				case Xamarin.Forms.TextAlignment.Center:
+					return System.Windows.TextAlignment.Center;
+				case Xamarin.Forms.TextAlignment.End:
+					return System.Windows.TextAlignment.Right;
+				default:
+					return System.Windows.TextAlignment.Left;
+			}
+		}
+
+		internal static VerticalAlignment ToNativeVerticalAlignment(this Xamarin.Forms.TextAlignment alignment)
+		{
+			switch (alignment)
+			{
+				case Xamarin.Forms.TextAlignment.Start:
+					return VerticalAlignment.Top;
+				case Xamarin.Forms.TextAlignment.Center:
+					return VerticalAlignment.Center;
+				case Xamarin.Forms.TextAlignment.End:
+					return VerticalAlignment.Bottom;
+				default:
+					return VerticalAlignment.Top;
+			}
+		}
+
+		internal static VerticalAlignment ToNativeVerticalAlignment(this LayoutOptions alignment)
+		{
+			switch (alignment.Alignment)
+			{
+				case LayoutAlignment.Start:
+					return VerticalAlignment.Top;
+				case LayoutAlignment.Center:
+					return VerticalAlignment.Center;
+				case LayoutAlignment.End:
+					return VerticalAlignment.Bottom;
+				case LayoutAlignment.Fill:
+					return VerticalAlignment.Stretch;
+				default:
+					return VerticalAlignment.Stretch;
+			}
+		}
+
+		internal static HorizontalAlignment ToNativeHorizontalAlignment(this LayoutOptions alignment)
+		{
+			switch (alignment.Alignment)
+			{
+				case LayoutAlignment.Start:
+					return HorizontalAlignment.Left;
+				case LayoutAlignment.Center:
+					return HorizontalAlignment.Center;
+				case LayoutAlignment.End:
+					return HorizontalAlignment.Right;
+				case LayoutAlignment.Fill:
+					return HorizontalAlignment.Stretch;
+				default:
+					return HorizontalAlignment.Stretch;
+			}
+		}
+	}
+
+	public static class FontExtensions
+	{
+		internal static void ApplyFont(this Control self, IFontElement element)
+		{
+			self.FontSize = element.FontSize;
+
+			if (!string.IsNullOrEmpty(element.FontFamily))
+				self.FontFamily = new FontFamily(new Uri("pack://application:,,,/"), element.FontFamily);
+			else
+				self.FontFamily = (FontFamily)System.Windows.Application.Current.Resources["FontFamilySemiBold"];
+
+			if (element.FontAttributes.HasFlag(FontAttributes.Italic))
+				self.FontStyle = FontStyles.Italic;
+			else
+				self.FontStyle = FontStyles.Normal;
+
+			if (element.FontAttributes.HasFlag(FontAttributes.Bold))
+				self.FontWeight = FontWeights.Bold;
+			else
+				self.FontWeight = FontWeights.Normal;
+		}
+
+		internal static double GetFontSize(this NamedSize size)
+		{
+			switch (size)
+			{
+				case NamedSize.Default:
+					return (double)System.Windows.Application.Current.Resources["ControlContentThemeFontSize"];
+				case NamedSize.Micro:
+					return (double)System.Windows.Application.Current.Resources["FontSizeSmall"] - 3;
+				case NamedSize.Small:
+					return (double)System.Windows.Application.Current.Resources["FontSizeSmall"];
+				case NamedSize.Medium:
+					return (double)System.Windows.Application.Current.Resources["FontSizeNormal"];
+				// use normal instead of medium as this is the default
+				case NamedSize.Large:
+					return (double)System.Windows.Application.Current.Resources["FontSizeLarge"];
+				default:
+					throw new ArgumentOutOfRangeException("size");
+			}
+		}
+
+		internal static bool IsDefault(this IFontElement self)
+		{
+			return self.FontFamily == null && self.FontSize == Device.GetNamedSize(NamedSize.Default, typeof(Label), true) && self.FontAttributes == FontAttributes.None;
+		}
+	}
+}
